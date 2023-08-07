b2c8632de9aada0abfb3af52a69700ac27886272
Jesus <jesus10.hn@gmail.com>
2023-08-07 17:32:51 -0600

    add filter

--- components/filters/Filter1GrainTexture.tsx.tsx
+++ components/filters/Filter1GrainTexture.tsx.tsx
@@ -4,18 +4,17 @@ import React from "react";
 
 export default function Filter1GrainTexture() {
   return (
-    <div className=" opacity-80">
-      <svg id="texture" className=" filter2">
+    <div className="opacity-80">
+      <svg id="texture" className="filter2">
         <filter id="noise">
           <feTurbulence
             type="fractalNoise"
-            baseFrequency=".8"
-            numOctaves="4"
+            baseFrequency="0.5"
+            numOctaves="2"
             stitchTiles="stitch"
-          ></feTurbulence>
-          <feColorMatrix type="saturate" values="0"></feColorMatrix>
+          />
         </filter>
-        <rect width="100%" height="100%" filter="url(#noise)"></rect>
+        <rect width="100%" height="100%" filter="url(#noise)" />
       </svg>
     </div>
   );
--- components/project.tsx
+++ components/project.tsx
@@ -25,12 +25,12 @@ export default function Project({
         priority // Prioriza la carga de la imagen
         className=" c-lesPJm-ikzLvCr-css"
       />
-      {/* <Filter1GrainTexture /> */}
+      <Filter1GrainTexture />
       <Filter2DiagonalLines />
       {/* <Filter3Colors /> */}
 
-      <div className="overflow-hidden relative   rounded-lg p-4 ">
-        <div className=" relative z-10 flex flex-col items-center gap-4 ">
+      <div className="overflow-hidden relative   rounded-lg p-4 bg-[rgba(255,255,255,.2)] ">
+        <div className=" relative z-10 flex flex-col items-center gap-4  ">
           {/* <img
             src={imageUrl}
             alt="Card Image"
