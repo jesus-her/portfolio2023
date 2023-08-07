3169695eaa44c78970ea01edb6f25b3146146332
Jesus <jesus10.hn@gmail.com>
2023-08-07 15:20:49 -0600

    change img

--- components/project.tsx
+++ components/project.tsx
@@ -15,16 +15,31 @@ export default function Project({
 }: ProjectProps) {
   return (
     <div className="   bg-[rgba(255,255,255,1)]   relative rounded-lg break-inside overflow-hidden m-1 mt-0">
-      <img alt="ola" src={imageUrl} className=" c-lesPJm-ikzLvCr-css" />
+      {/* <img alt="ola" src={imageUrl} className=" c-lesPJm-ikzLvCr-css" /> */}
+      <Image
+        src={imageUrl}
+        alt="Project Image"
+        priority // Prioriza la carga de la imagen
+        className=" c-lesPJm-ikzLvCr-css"
+      />
       {/* <Filter1GrainTexture /> */}
 
       <div className="overflow-hidden relative   rounded-lg p-4 bg-[rgba(255,255,255,0.2)] ">
         <div className=" relative z-10 flex flex-col items-center gap-4 ">
-          <img
+          {/* <img
             src={imageUrl}
             alt="Card Image"
             className=" w-72 h-auto  object-cover  rounded-lg"
+          /> */}
+          <Image
+            src={imageUrl}
+            alt="Project Image"
+            width={800} // Establece el ancho de la imagen
+            height={600} // Establece la altura de la imagen
+            layout="responsive" // Permite que la imagen se ajuste al contenedor
+            priority // Prioriza la carga de la imagen
           />
+
           <div className="flex flex-wrap gap-2">
             {tags.map((tag: string) => (
               <div
@@ -47,12 +62,12 @@ export default function Project({
             className="group px-7 py-3 text-white
                    flex items-center gap-2 rounded-full shadow-lg
                  
-                   outline-none  hover:opacity-70 bg-gradient-to-r from-[#000] via-[#000] to-[#000]
+                   outline-none  hover:opacity-70 bg-black
                    
                    "
             onClick={() => {}}
           >
-            Discover this project
+            Discover this project ch
           </Link>
         )}
       </div>
--- lib/data.ts
+++ lib/data.ts
@@ -10,6 +10,14 @@ import { LuGraduationCap } from "react-icons/lu";
 
 import { FaHome } from "react-icons/fa";
 import { BsMailbox, BsPuzzleFill, BsStack } from "react-icons/bs";
+import finanz from "@/public/finanz.jpg";
+import lilpix from "@/public/lilpix.jpg";
+import parkupadmin from "@/public/parkupadmin.jpg";
+import quizzed from "@/public/quizzed.png";
+import estudihambre from "@/public/estudihambre.jpg";
+import parkupappimg from "@/public/parkupapp.jpg";
+import artist from "@/public/artist.jpg";
+import lilpixweb from "@/public/lilpixweb.jpg";
 import { HiHome } from "react-icons/hi";
 
 export const links = [
@@ -74,14 +82,13 @@ export const experiencesData = [
     date: "2022 - present",
   },
 ] as const;
-
 export const projectsData = [
   {
     title: "Fintech App",
     description:
       "A UI/UX mobile app, made with React Native (Expo CLI). It is an application created to make your investment more comfortable to manage, without long explanations, with a portfolio of modern instruments trying to balance your risk. It is an application with which you can automate your purchases and sales of cryptocurrencies, besides that, it includes the benefits of a Fintech and connects you directly to the website 'CETES'.",
     tags: ["React Native", "Expo CLI", "TypeScript"],
-    imageUrl: " https://i.imgur.com/T2gBwxfh.png",
+    imageUrl: finanz,
     date: "2021 - present",
     link: "https://github.com/jesus-her/finanz",
   },
@@ -90,7 +97,7 @@ export const projectsData = [
     description:
       "A mobile application, made with React Native in which you can download my wallpapers.",
     tags: ["React Native", "Expo CLI", "Strapi"],
-    imageUrl: "https://i.imgur.com/GSZUID3h.png",
+    imageUrl: lilpix,
     date: "2021 - present",
     link: "https://github.com/jesus-her/the_lilpix_app",
   },
@@ -107,7 +114,7 @@ export const projectsData = [
       "TypeScript",
       "Material UI",
     ],
-    imageUrl: "https://i.imgur.com/H5iZAPAh.png",
+    imageUrl: parkupadmin,
     date: "2021 - present",
     link: "https://parkupadmin.blackcloud.app/",
   },
@@ -116,7 +123,7 @@ export const projectsData = [
     description:
       "A fully functional mobile app, made with React Native and firebase. It is an application in which you can create your own quiz and it will be published for everyone who has the app, you can play and answer other people's quizzes and get a place on the leaderboar.",
     tags: ["React Native", "Expo CLI", "Firebase", "Typescript"],
-    imageUrl: "https://i.imgur.com/Xf2Dvg0h.png",
+    imageUrl: quizzed,
     date: "2021 - present",
     link: "https://github.com/jesus-her/MeetMe",
   },
@@ -125,7 +132,7 @@ export const projectsData = [
     description:
       "Web app to publish and find food in the University. Helps students to discover variety of products that other students offers in the University and also make it easy to publish any food or product.",
     tags: ["React", "Next.js", "Next Auth", "Tailwindcss", "TypeScript"],
-    imageUrl: "https://i.imgur.com/YGRrWy7h.png",
+    imageUrl: estudihambre,
     date: "2021 - present",
     link: "https://estudihambre.vercel.app/",
   },
@@ -143,7 +150,7 @@ export const projectsData = [
       "TypeScript",
       "Nativebase",
     ],
-    imageUrl: "https://i.imgur.com/Y0m4s0hh.jpg",
+    imageUrl: parkupappimg,
     date: "2021 - present",
     link: "",
   },
@@ -153,7 +160,7 @@ export const projectsData = [
     description:
       "In my free time I like to learn a variety of things, among them, digital art. I started with photography, then I moved on to image manipulation with Adobe Photoshop, and today I am in the world of 3D graphics using Blender, which together with Photoshop has helped me to create everything that is shown in the @lilpix_px.",
     tags: ["instagram: @lilpix_px"],
-    imageUrl: "https://i.imgur.com/2bMFTmHl.png",
+    imageUrl: artist,
     date: "2021 - present",
     link: "https://www.instagram.com/lilpix_px/",
   },
@@ -162,11 +169,104 @@ export const projectsData = [
     description:
       "My really first web app using React. This website is a image gallery of my artwork",
     tags: ["React", "CSS", "Strapi"],
-    imageUrl: "https://i.imgur.com/FTqP56th.png",
+    imageUrl: lilpixweb,
     date: "2021 - present",
     link: "https://lilpix.netlify.app/",
   },
 ] as const;
+
+// export const projectsData = [
+//   {
+//     title: "Fintech App",
+//     description:
+//       "A UI/UX mobile app, made with React Native (Expo CLI). It is an application created to make your investment more comfortable to manage, without long explanations, with a portfolio of modern instruments trying to balance your risk. It is an application with which you can automate your purchases and sales of cryptocurrencies, besides that, it includes the benefits of a Fintech and connects you directly to the website 'CETES'.",
+//     tags: ["React Native", "Expo CLI", "TypeScript"],
+//     imageUrl: " https://i.imgur.com/T2gBwxfh.png",
+//     date: "2021 - present",
+//     link: "https://github.com/jesus-her/finanz",
+//   },
+//   {
+//     title: "Wallpaper App",
+//     description:
+//       "A mobile application, made with React Native in which you can download my wallpapers.",
+//     tags: ["React Native", "Expo CLI", "Strapi"],
+//     imageUrl: "https://i.imgur.com/GSZUID3h.png",
+//     date: "2021 - present",
+//     link: "https://github.com/jesus-her/the_lilpix_app",
+//   },
+//   {
+//     title: "ParkUp Dashboard",
+//     description:
+//       "Web app to manage all the data of ParkUp App. ParkUp is a mobile application developed to streamline the parking experience.  ",
+//     tags: [
+//       "React",
+//       "MongoDB",
+//       "Node js",
+//       "Express",
+//       "DigitalOcean",
+//       "TypeScript",
+//       "Material UI",
+//     ],
+//     imageUrl: "https://i.imgur.com/H5iZAPAh.png",
+//     date: "2021 - present",
+//     link: "https://parkupadmin.blackcloud.app/",
+//   },
+//   {
+//     title: "Quizzed App",
+//     description:
+//       "A fully functional mobile app, made with React Native and firebase. It is an application in which you can create your own quiz and it will be published for everyone who has the app, you can play and answer other people's quizzes and get a place on the leaderboar.",
+//     tags: ["React Native", "Expo CLI", "Firebase", "Typescript"],
+//     imageUrl: "https://i.imgur.com/Xf2Dvg0h.png",
+//     date: "2021 - present",
+//     link: "https://github.com/jesus-her/MeetMe",
+//   },
+//   {
+//     title: "Estudihambre",
+//     description:
+//       "Web app to publish and find food in the University. Helps students to discover variety of products that other students offers in the University and also make it easy to publish any food or product.",
+//     tags: ["React", "Next.js", "Next Auth", "Tailwindcss", "TypeScript"],
+//     imageUrl: "https://i.imgur.com/YGRrWy7h.png",
+//     date: "2021 - present",
+//     link: "https://estudihambre.vercel.app/",
+//   },
+//   {
+//     title: "ParkUp",
+//     description:
+//       "Simple and user friendly parking payment application that saves our users both time and money. ParkUp is LA’s premier and largest network smart parking platform.",
+//     tags: [
+//       "React Native",
+//       "Firebase",
+//       "MongoDB",
+//       "Node js",
+//       "Express",
+//       "DigitalOcean",
+//       "TypeScript",
+//       "Nativebase",
+//     ],
+//     imageUrl: "https://i.imgur.com/Y0m4s0hh.jpg",
+//     date: "2021 - present",
+//     link: "",
+//   },
+
+//   {
+//     title: "Photograph and 3D Art",
+//     description:
+//       "In my free time I like to learn a variety of things, among them, digital art. I started with photography, then I moved on to image manipulation with Adobe Photoshop, and today I am in the world of 3D graphics using Blender, which together with Photoshop has helped me to create everything that is shown in the @lilpix_px.",
+//     tags: ["instagram: @lilpix_px"],
+//     imageUrl: "https://i.imgur.com/2bMFTmHl.png",
+//     date: "2021 - present",
+//     link: "https://www.instagram.com/lilpix_px/",
+//   },
+//   {
+//     title: "Lilpix",
+//     description:
+//       "My really first web app using React. This website is a image gallery of my artwork",
+//     tags: ["React", "CSS", "Strapi"],
+//     imageUrl: "https://i.imgur.com/FTqP56th.png",
+//     date: "2021 - present",
+//     link: "https://lilpix.netlify.app/",
+//   },
+// ] as const;
 
 export const skillsData = [
   "HTML",
