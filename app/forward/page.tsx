96e7ad6d4b2f37758030786ea7d6268d7ad4f109
Jesús Hernández <jesus10.hn@gmail.com>
2024-01-26 17:41:29 -0600

    test

--- app/forward/page.tsx
+++ app/forward/page.tsx
@@ -1,22 +1,9 @@
-// pages/api/payment-response.js
+import { headers } from 'next/headers'
 
-export default function handler (req: any, res: any) {
-  // Next.js API route body parsing is automatic, so you can access req.body directly
-  if (req.method === 'POST') {
-    // Aquí manejas la respuesta. Puedes obtener los headers así:
-    const headers = req.headers
+export default function Page () {
+  const headersList = headers()
+  const referer = headersList.get('referer')
+  console.log(headers)
 
-    // Logica para procesar los datos de la respuesta
-    console.log('HEADERS', headers)
-
-    // ...
-
-    // Finalmente, envía una respuesta al cliente
-    //   return res.status(200).json({ message: 'Respuesta recibida y procesada' })
-  } else {
-    // Método HTTP no soportado
-    console.log('not supported')
-
-    //   return res.status(405).end()
-  }
+  return <div>Referer: {referer}</div>
 }
